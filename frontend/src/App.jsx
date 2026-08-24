142fc5ba1ef201f32541c39a4bd84e5a744ba5e7
Vinson <vinsonhe2@gmail.com>
2026-08-24 12:11:52 -0700

    style(navbar): add basic navbar styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,73 @@
 import { Link } from "react-router";
 import { logoSvg } from "./assets/index.js";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <>
-      <nav>
+      <nav className={styles.navbar}>
         <ul>
           <li>
-            <div>
+            <Link to="/" className={styles.logoLink}>
               <img src={logoSvg} width="40" height="40" alt="Logo" />
-              <Link to="/">Message</Link>
-            </div>
-          </li>
-          <li>
-            <Link to="/">
-              <span className="material-symbols-outlined">home</span>
-              Home
+              <span>Message</span>
             </Link>
           </li>
-          <li className="messagesList">
-            <ul>
+          <li className={styles.messagesParentLi}>
+            <ul className={styles.messagesUl}>
               <li>
-                <img
-                  src="https://birdlifedata.blob.core.windows.net/species-images/22697748.jpg"
-                  alt="Profile picture"
-                  width="40"
-                />
-                <p>Random Kasinski</p>
+                <Link to="/" className={styles.homeLink}>
+                  <span
+                    className={`material-symbols-outlined ${styles.homeIcon}`}
+                  >
+                    home
+                  </span>
+                  Home
+                </Link>
               </li>
-              <li>You have no messages right now.</li>
               <li>
-                <button type="button">
-                  <span className="material-symbols-outlined">add</span>
+                <Link to="/">
+                  <img
+                    src="https://birdlifedata.blob.core.windows.net/species-images/22697748.jpg"
+                    alt="Profile picture"
+                    width="32"
+                    className={styles.profilePicture}
+                  />
+                  <p>Random Kasinski</p>
+                </Link>
+              </li>
+              <li>
+                <p className={styles.emptyChatsPara}>
+                  You have no chats right now.
+                </p>
+              </li>
+              <li>
+                <button type="button" className={styles.createButton}>
+                  <span
+                    className={`material-symbols-outlined ${styles.createIcon}`}
+                  >
+                    add
+                  </span>
                   Create a new chat
                 </button>
               </li>
             </ul>
           </li>
-          <li>
-            <img src="" alt="Your profile picture" width="40" />
+          <li className={styles.profileLi}>
+            <img
+              src="https://i.pinimg.com/236x/13/74/20/137420f5b9c39bc911e472f5d20f053e.jpg"
+              alt="Your profile picture"
+              width="40"
+              className={styles.profilePicture}
+            />
             <p>Test Profile</p>
-            <span className="material-symbols-outlined">
-              keyboard_arrow_down
-            </span>
+            <button type="button" className={styles.dropdownButton}>
+              <span
+                className={`material-symbols-outlined ${styles.dropdownIcon}`}
+              >
+                keyboard_arrow_down
+              </span>
+            </button>
             {/* profile settings here */}
           </li>
         </ul>
