6a644cf6f2f769ad1abe71c594d613d15cc6a97f
Vinson <vinsonhe2@gmail.com>
2026-09-04 12:03:05 -0700

    style(auth): add sign up responsive styling

--- frontend/src/signUp/SignUp.jsx
+++ frontend/src/signUp/SignUp.jsx
@@ -18,45 +18,47 @@ const SignIn = () => {
         />
       </div>
       <section className={styles.formSection}>
-        <h1>Connect to the World</h1>
-        <p>
-          The advent of the internet has connected us more than the biggest of
-          expectations. Join in on the fun and create an account.
-        </p>
-        <form method="POST">
-          <div>
-            <label for="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              minLength="3"
-              maxLength="254"
-              required
-            />
-          </div>
-          <div>
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required />
-          </div>
-          <div>
-            <label for="username">Username</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              minLength="3"
-              maxLength="64"
-              required
-            />
-          </div>
-          <div>
-            <button type="submit">Sign Up</button>
-          </div>
-        </form>
-        <p className={styles.signInPara}>
-          Already have an account? <Link to="/sign-in">Sign in</Link>
-        </p>
+        <div className={styles.formContainer}>
+          <h1>Connect to the World</h1>
+          <p>
+            The advent of the internet has connected us more than the biggest of
+            expectations. Join in on the fun and create an account.
+          </p>
+          <form method="POST">
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                minLength="3"
+                maxLength="254"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" name="password" required />
+            </div>
+            <div>
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                minLength="3"
+                maxLength="64"
+                required
+              />
+            </div>
+            <div>
+              <button type="submit">Sign Up</button>
+            </div>
+          </form>
+          <p className={styles.signInPara}>
+            Already have an account? <Link to="/sign-in">Sign in</Link>
+          </p>
+        </div>
       </section>
     </div>
   );
