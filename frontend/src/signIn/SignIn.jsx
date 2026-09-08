922a81a64924bbfebecbdd9ec4e0779b6ed8200d
Vinson <vinsonhe2@gmail.com>
2026-09-08 13:34:09 -0700

    style(auth): add sign in responsive styling

--- frontend/src/signIn/SignIn.jsx
+++ frontend/src/signIn/SignIn.jsx
@@ -5,25 +5,26 @@ import styles from "./SignIn.module.css";
 const SignIn = () => {
   return (
     <div className={styles.pageContainer}>
-      <div className={styles.logoContainer}>
-        <img src={logoSvg} width="40" height="40" alt="Logo" />
-        <span>Message</span>
+      <div className={styles.formContainer}>
+        <div className={styles.logoContainer}>
+          <img src={logoSvg} width="40" height="40" alt="Logo" />
+        </div>
+        <h1>Sign In</h1>
+        <form method="post">
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" name="email" required />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <button type="submit">Sign in</button>
+        </form>
+        <p className={styles.signInPara}>
+          Don't have an account yet? <Link to="/sign-up">Sign up</Link>
+        </p>
       </div>
-      <h1>Sign In</h1>
-      <form method="post">
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="text" id="email" name="email" required />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" required />
-        </div>
-        <button type="submit">Sign in</button>
-      </form>
-      <p className={styles.signInPara}>
-        Don't have an account yet? <Link to="/sign-in">Sign up</Link>
-      </p>
     </div>
   );
 };
