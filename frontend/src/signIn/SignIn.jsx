dcb6cef12bbdc4a5d27711749e7ffa1b5da95abd
Vinson <vinsonhe2@gmail.com>
2026-09-15 14:30:36 -0700

    feat(auth): add sign in functionality
    - Validate form on submit and refresh the page with errors if applicable
    - On successful form submit, sign the user in and redirect them to
      the homepage
    - Add error styling and polish to the sign in page

--- frontend/src/signIn/SignIn.jsx
+++ frontend/src/signIn/SignIn.jsx
@@ -1,8 +1,17 @@
-import { Link } from "react-router";
+import { Link, useFetcher } from "react-router";
 import { logoSvg } from "../assets";
 import styles from "./SignIn.module.css";
 
+const InputErrorPara = ({ message }) => {
+  console.log(message);
+  return <p className={styles.inputErrorPara}>{message}</p>;
+};
+
 const SignIn = () => {
+  const fetcher = useFetcher();
+
+  const isValid = fetcher.data?.status === "success";
+
   return (
     <div className={styles.pageContainer}>
       <div className={styles.formContainer}>
@@ -10,17 +19,32 @@ const SignIn = () => {
           <img src={logoSvg} width="40" height="40" alt="Logo" />
         </div>
         <h1>Sign In</h1>
-        <form method="post">
+        <fetcher.Form method="post" noValidate>
+          {fetcher.data && !isValid ? (
+            <InputErrorPara message={fetcher.data.errors[0].message} />
+          ) : null}
           <div>
             <label htmlFor="email">Email</label>
-            <input type="text" id="email" name="email" required />
+            <input
+              type="text"
+              id="email"
+              name="email"
+              className={fetcher.data && !isValid ? styles.invalid : null}
+              required
+            />
           </div>
           <div>
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" required />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className={fetcher.data && !isValid ? styles.invalid : null}
+              required
+            />
           </div>
           <button type="submit">Sign in</button>
-        </form>
+        </fetcher.Form>
         <p className={styles.signInPara}>
           Don't have an account yet? <Link to="/sign-up">Sign up</Link>
         </p>
