c3ad7c92150a0e05a0084cd62cd08aac0e412b37
Vinson <vinsonhe2@gmail.com>
2026-09-21 08:14:54 -0700

    style(message): add message form styling

--- frontend/src/messageForm/MessageForm.jsx
+++ frontend/src/messageForm/MessageForm.jsx
@@ -3,7 +3,7 @@ import styles from "./MessageForm.module.css";
 
 const MessageForm = () => {
   const [users, setUsers] = useState(null);
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(null);
 
   // fetch all users
   useEffect(() => {
@@ -28,38 +28,57 @@ const MessageForm = () => {
     fetchData();
   }, []);
 
+  function handleInputEnter(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+    }
+  }
+
   // make sure to not send a new message request if users are already messaging!
+  // also add search functionality
+
+  // and finally, we need to first fix the overlay issue and then submit the styling commit!
 
   return (
     <div className={styles.messageFormContainer}>
-      <div className={styles.messageForm}>
+      <div className={styles.messageDiv}>
         <h1>Create a New Message Chat</h1>
-        <form>
-          <input type="text" id="search" name="search" />
+        <form className={styles.messageForm}>
+          <input
+            type="text"
+            id="search"
+            name="search"
+            onKeyDown={handleInputEnter}
+          />
           <input type="hidden" id="targetUser" name="targetUser" value="" />
+          {error ? (
+            <p className={styles.errorPara}>
+              There was an error getting the users. Please try again later.
+            </p>
+          ) : null}
+          {users && users.length > 0 ? (
+            <ul className={styles.usersUl}>
+              {users.map((user) => (
+                <li data-id={user.id} key={user.id}>
+                  <img
+                    src={user.profile}
+                    alt={`${user.username}'s profile picture`}
+                    width="32"
+                    className={styles.profilePicture}
+                  />
+                  <span>{user.username}</span>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>There are no users right now!</p>
+          )}
+          <div>
+            <button type="submit" className={styles.submitButton}>
+              Send Request
+            </button>
+          </div>
         </form>
-        {error ? (
-          <p className={styles.errorPara}>
-            There was an error getting the users. Please try again later.
-          </p>
-        ) : null}
-        {users && users.length > 0 ? (
-          <ul className={styles.usersUl}>
-            {users.map((user) => (
-              <li data-id={user.id} key={user.id}>
-                <img
-                  src={user.profile}
-                  alt={`${user.username}'s profile picture`}
-                  width="32"
-                  className={styles.profilePicture}
-                />
-                <span>{user.username}</span>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>There are no users right now!</p>
-        )}
       </div>
     </div>
   );
